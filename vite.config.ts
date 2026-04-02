db55ff010f8a97f5dcce9371cf91cbcc55923c82
Kevin Mak <makkevin08@gmail.com>
2026-04-02 13:07:34 -0600

    not yet

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/Kevy9099/job-tracker-application/'
+  base: '/job-tracker-application/'
 })
